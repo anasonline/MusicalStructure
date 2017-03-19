295fe535a38f9db1d56ccf44328bcada816cc297
Anas <anas.marrawi@gmail.com>
2017-03-19 12:15:01 +0100

    Attach an OnClick listener to the Now Playing button and implement the Now Playing layout

--- app/src/main/java/com/example/android/musicalstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicalstructure/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicalstructure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the Now Playing view
+        TextView nowPlaying = (TextView) findViewById(R.id.now_playing);
+
+        // Set a ClickListener on the Now Playing view
+        nowPlaying.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlayingActivity.class);
+                startActivity(nowPlayingIntent);
+            }
+        });
+
     }
 }
